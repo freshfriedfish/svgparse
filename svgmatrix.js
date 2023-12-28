75e15971728c7d42324dd225298a8a9535d3bf8c
freshfriedfish <shafstablet@gmail.com>
2023-12-27 23:56:44 -0500

    progress and testing stuff

--- svgmatrix.js
+++ svgmatrix.js
@@ -5,6 +5,11 @@ import {Matrix} from "ml-matrix";
 const path = document.getElementById('path2');
 const pathdata = path.getPathData({normalize:true});
 console.log(pathdata);
+//test pathdata 1darr
+const flatarr = pathdata.map((command) => {
+    return command.values;
+}).flat(1);
+console.log(flatarr);
 
 const aMatrix = new Matrix([
     [],
@@ -20,17 +25,60 @@ pathdata.forEach((command) => {
 console.log(aMatrix.to2DArray());
 const startPos = aMatrix.getColumnVector(0);
 // aMatrix.subColumnVector(startPos);
-console.log(aMatrix.to2DArray());
+console.log(aMatrix.transpose().to2DArray());
+//maparr length for loop
+
+//test map arr arr
+const arrarr = pathdata.map((command) => {
+    return command.values;
+});
+console.log(arrarr);
+
+//test map arr arr to arr mat
+
+const arrmat = arrarr.map((element) => {
+    return arr2mat(element);
+});
+arrmat.forEach((mat) => console.log(mat.to2DArray()));
+
+//test map mat arr
+const matarr = pathdata.map((command) => {
+    return new Matrix([command.values]);
+});
+matarr.forEach((mat) => console.log(mat.to2DArray()));
+
+//matrix remove test
+
 
 //matrix to pathdata
 const arr = [];
-pathdata.forEach((command, index) => {
+let count = 0;
+pathdata.forEach((command) => {
     const thearr = [];
-    for (let i = 0; i < command.values.length-1; i++) {
-        thearr.push(aMatrix.getColumn(i));
+
+    switch (command) {
+        case "M":
+
+
     }
+
     arr.push(thearr);
 });
 
-console.log(arr);
-console.log(aMatrix.to2DArray());
+// console.log(arr);
+
+/**
+ *
+ * @param arr
+ * @returns {Matrix}
+ */
+function arr2mat(arr) {
+    const mat = new Matrix([
+        [],
+        [],
+    ]);
+    for (let i = 0; i < arr.length; i+=2) {
+        mat.addColumn([arr[i], arr[i + 1]]);
+    }
+    return mat;
+}
