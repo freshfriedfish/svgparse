fb5bf2d066abef0baf22b60a74133ff0096885ca
freshfriedfish <shafayatm2@gmail.com>
2024-01-10 02:56:09 -0500

    path attribute parsed to objects

--- pathparsing.js
+++ pathparsing.js
@@ -10,6 +10,10 @@ const parsedPath = parse("M43 110C100 69 167 167 226 116C318 183 372 75 350 43")
 console.log(parsedPath);
 
 const commands = parsedPath.map((elem) => elem.shift());
-
 console.log(commands);
-console.log(parsedPath);
+const arrlen = parsedPath.map((elem) => elem.length / 2 -1);
+console.log(arrlen);
+const flatArr = parsedPath.map((elem) => elem).flat(1);
+console.log(flatArr)
+
+console.log(bruh(flatArr));
